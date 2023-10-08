ea1de03b6749af2e8dc4b143ba3f50486845be1c
Doc Pillman <doc.pillman@icloud.com>
2023-10-09 01:46:57 +0300

    again fix refactoring error

--- storage/storage_mcu.c
+++ storage/storage_mcu.c
@@ -92,7 +92,7 @@ static bool flash_erase(struct storage_element* instance)
 
     if (HAL_FLASHEx_Erase(&erase_struct, &page_error) != HAL_OK)
     {
-        instance->state = rstorage_error;
+        instance->state = storage_error;
         return false;
     }
 
@@ -146,7 +146,7 @@ bool storage_write(struct storage_element* instance, void* data, uint32_t bytes)
         if (HAL_FLASH_Program(FLASH_TYPEPROGRAM_DOUBLEWORD, address, *data_in_portions_type) != HAL_OK)
         {
             HAL_FLASH_Lock();
-            instance->state = rstorage_error;
+            instance->state = storage_error;
             return false;
         }
         data_in_portions_type++;
