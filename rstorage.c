97c26555e87cba6711808aad19830a0f34e4f94b
Firsov Pavel <pfirsov@ozon.ru>
2023-10-09 11:42:33 +0300

    Del trush

--- rstorage.c
+++ rstorage.c
@@ -7,13 +7,7 @@ bool rstorage_init(rstorage* instance, int size_kbytes)
     if (size_kbytes == 0 || size_kbytes > MAX_SIZE_STORAGE_KBYTES || instance == NULL)
         return false;
 
-#if defined(RSTORAGE_USING_FD)
     return storage_init(instance, size_kbytes);
-#endif
-
-#if defined(STM32G474xx) || defined(STM32F103xB)
-    return storage_init(instance, size_kbytes);
-#endif
 }
 
 bool rstorage_write(rstorage* instance, void* data, uint32_t bytes)
@@ -21,13 +15,7 @@ bool rstorage_write(rstorage* instance, void* data, uint32_t bytes)
     if (instance == NULL || data == NULL)
         return false;
 
-#if defined(RSTORAGE_USING_FD)
     return storage_write(instance, data, bytes);
-#endif
-
-#if defined(STM32G474xx) || defined(STM32F103xB)
-    return storage_write(instance, data, bytes);
-#endif
 }
 
 bool rstorage_read(rstorage* instance, void* data, uint32_t bytes)
@@ -35,11 +23,5 @@ bool rstorage_read(rstorage* instance, void* data, uint32_t bytes)
     if (instance == NULL || data == NULL)
         return false;
 
-#if defined(RSTORAGE_USING_FD)
     return storage_read(instance, data, bytes);
-#endif
-
-#if defined(STM32G474xx) || defined(STM32F103xB)
-    return storage_read(instance, data, bytes);
-#endif
 }
